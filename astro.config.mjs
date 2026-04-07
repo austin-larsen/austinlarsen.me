b8cc0b14bbc100298a6fa4478dea1dc306f81471
Austin Larsen <55168932+austin-larsen@users.noreply.github.com>
2026-04-06 23:50:04 -0400

    enable allowDangerousHTML for iframe embeds in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://austinlarsen.me',
   integrations: [sitemap()],
+  markdown: {
+    allowDangerousHTML: true,
+  },
 });
